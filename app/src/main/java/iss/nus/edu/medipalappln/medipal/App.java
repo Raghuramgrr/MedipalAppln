0605513814b1fb311c79aa0010a1203832d8118e
evon <evonwoo84@gmail.com>
2017-03-21 16:52:37 +0800

    Create user object in App class

--- app/src/main/java/iss/nus/edu/medipalappln/medipal/App.java
+++ app/src/main/java/iss/nus/edu/medipalappln/medipal/App.java
@@ -1,13 +1,20 @@
 package iss.nus.edu.medipalappln.medipal;
 
 import android.app.Application;
+import android.util.Log;
 
 public class App extends Application {
+    private static final String TAG = "App";
     public static User user;
+
+    //TODO: link session to user object
+    //public Session session
 
     @Override
     public void onCreate() {
         super.onCreate();
         user = new User();
+        //user = new User(session.username());
+        Log.i(TAG, "User: XXX" + user.getUserIDNo());
     }
 }
